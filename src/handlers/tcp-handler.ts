28856a4c77438b7839c58a16d99fbdd188462e3f
tientran1234 <tranngoctien29112003@gmail.com>
2025-07-21 20:36:12 +0700

    log bug

--- src/handlers/tcp-handler.ts
+++ src/handlers/tcp-handler.ts
@@ -50,6 +50,7 @@ export async function handleTCPRequest(payload: any): Promise<HandleTCPReturn> {
             statusCode,
             timestamp: new Date().toISOString(),
         };
+console.log(result);
 
         return result;
 
